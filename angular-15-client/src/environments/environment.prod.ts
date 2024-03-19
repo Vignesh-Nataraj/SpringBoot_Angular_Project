d4e74a23aad9c12e18b53b164d378618ceffb3b3
Vignesh N <vigneshvicky921@gmail.com>
2024-03-19 12:08:12 +0530

    Update environment.prod.ts

--- angular-15-client/src/environments/environment.prod.ts
+++ angular-15-client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://springbootangularproject.s3.ap-south-1.amazonaws.com/spring-boot-docker.jar'
+    baseUrl: 'ec2-13-126-181-156.ap-south-1.compute.amazonaws.com:8080/api/tutorials'
   };
