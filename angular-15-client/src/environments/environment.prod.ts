ab714fd8ad08474c275779657b2847665679c7a9
Vignesh N <vigneshvicky921@gmail.com>
2024-03-20 12:12:39 +0530

    Update environment.prod.ts

--- angular-15-client/src/environments/environment.prod.ts
+++ angular-15-client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://ec2-13-126-181-156.ap-south-1.compute.amazonaws.com:8080/api/tutorials'
+    baseUrl: 'http://ec2-65-0-209-57.ap-south-1.compute.amazonaws.com:8080/api/tutorials'
   };
